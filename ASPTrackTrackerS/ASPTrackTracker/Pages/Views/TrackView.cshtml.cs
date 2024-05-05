c65e353331bfd26bb40a979efbc15a7dd26478c6
Mati279 <104004760+Mati279@users.noreply.github.com>
2024-05-05 18:04:37 -0300

    Track Data to TrackView

--- ASPTrackTrackerS/ASPTrackTracker/Pages/Views/TrackView.cshtml.cs
+++ ASPTrackTrackerS/ASPTrackTracker/Pages/Views/TrackView.cshtml.cs
@@ -1,3 +1,4 @@
+using ASPTrackTracker.ScoreHelpers;
 using DataLibrary.Data;
 using DataLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +9,7 @@ namespace ASPTrackTracker.Pages.Views
 {
     public class TrackViewModel : PageModel
     {
+        private readonly ScoresManager scoresManager;
         private readonly ITrackData trackData;
         private readonly IUserData userData;
         private readonly IArtistData artistData;
@@ -23,6 +25,17 @@ namespace ASPTrackTracker.Pages.Views
         public string TrackGenre { get; set; }
         public string TrackStyle { get; set; }
 
+
+        public TrackModel Track {  get; set; }
+
+        public UserModel User {  get; set; }
+
+        public ArtistModel Artist {  get; set; }
+
+        public GenreModel Genre {  get; set; }
+
+        public StyleModel Style {  get; set; }
+
         public double AverageScore { get; set; }
         public double AffinityScore { get; set; }
         public double ComplexityScore { get; set; }
@@ -30,11 +43,12 @@ namespace ASPTrackTracker.Pages.Views
         public double VoicesScore { get; set; }
         public double LyricsScore { get; set; }
         public double InstrumentalScore { get; set; }
+        List<ScoreModel> TrackScores { get; set; }
+        
 
-        public TrackModel Track {  get; set; }
-
-        public TrackViewModel(ITrackData trackData, IUserData userData, IArtistData artistData, IGenreData genreData, IStyleData styleData, IScoreData scoreData)
+        public TrackViewModel(ScoresManager scoresManager, ITrackData trackData, IUserData userData, IArtistData artistData, IGenreData genreData, IStyleData styleData, IScoreData scoreData)
         {
+            this.scoresManager = scoresManager;
             this.trackData = trackData;
             this.userData = userData;
             this.artistData = artistData;
@@ -45,12 +59,68 @@ namespace ASPTrackTracker.Pages.Views
         public async Task OnGet()
         {
             Track = await trackData.GetById<TrackModel>(Id);
-            
+            User = await userData.GetById<UserModel>(Track.UserId);
+            Artist = await artistData.GetById<ArtistModel>(Track.ArtistId);
+            Genre = await genreData.GetById<GenreModel>(Track.GenreId);
+            Style = await styleData.GetById<StyleModel>(Track.StyleId);
+
+            TrackScores = await scoresManager.GetTrackScores(Track);
+
+            AverageScore = GetAverage();
+            AffinityScore = GetTrackScoreByStat("Affinity");
+            ComplexityScore = GetTrackScoreByStat("Complexity");
+            CreativityScore = GetTrackScoreByStat("Creativity");
+            VoicesScore = GetTrackScoreByStat("Voices");
+            LyricsScore = GetTrackScoreByStat("Lyrics");
+            InstrumentalScore = GetTrackScoreByStat("Instrumental");
+
         }
 
         public async Task OnPost()
         {
 
+        }
+
+        private double GetTrackScoreByStat(string Stat)
+        {
+            foreach(ScoreModel score in  TrackScores)
+            {
+                if(score.Stat == Stat)
+                {
+                    return score.Value;
+                }
+            }
+                    return 0;
+        }
+
+        private double GetAverage()
+        {
+            int count = 0;
+            double values = 0;
+
+            foreach (ScoreModel score in TrackScores)
+            {
+                if (score.Value != 0)
+                {
+                    values += score.Value;
+                    count++;
+                }
+            }
+            double average = values / count;
+
+            return  Math.Round(average, 2);
+        }
+
+        public string ReturnScoreOrDashes(double score)
+        {
+            if(score == 0)
+            {
+                return "- -";
+            }
+            else
+            {
+                return score.ToString();
+            }
         }
     }
 }
